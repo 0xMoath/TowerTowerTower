b16cd478aec3e3231277f53ec1afb03b73a03556
Student <student@lab-240-013.cs.csubak.edu>
2019-10-09 10:16:11 -0700

    fourStepsOfDebugging Updated

--- moathN.cpp
+++ moathN.cpp
@@ -1,3 +1,6 @@
+//Moath Nafjan
+//CMPS3350
+
 #include <iostream>
 
 #include <cstdlib>
@@ -6,6 +9,13 @@
 #include "fonts.h"
 #include <GL/glx.h>
 
+
+/* Four steps of Debugging of Gordons understanding 
+ *1- Reproduce the bug
+ *2- locate the bad code
+ *3- fix the code
+ *4- test and fix the whole program if needed, since they might some errors 		caused after fixing the bug
+ * */
 void MoathRend(int x, int y, Rect r)
 {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -20,3 +30,4 @@ void MoathRend(int x, int y, Rect r)
 	ggprint16(&r, 16, 0xfff0, "Moath's File");
 
 }
+
