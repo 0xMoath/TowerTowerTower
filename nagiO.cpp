a2ad987bd8bbd185ae61cf9c154c13964ac3e69a
thenagiobeid <thenagiobeid@gmail.com>
2019-10-27 17:53:27 -0700

    main img

--- nagiO.cpp
+++ nagiO.cpp
@@ -10,7 +10,97 @@
 #include <GL/glx.h>
 #include "fonts.h"
 #include <fstream>
+#include <cstring>
 using namespace std;
+
+class Image {
+public:
+	int width, height;
+	unsigned char *data;
+	~Image() { delete [] data; }
+	Image(const char *fname) {
+		if (fname[0] == '\0')
+			return;
+		int ppmFlag = 0;
+		char name[40];
+		strcpy(name, fname);
+		int slen = strlen(name);
+		char ppmname[80];
+		if (strncmp(name+(slen-4), ".ppm", 4) == 0)
+			ppmFlag = 1;
+		if (ppmFlag) {
+			strcpy(ppmname, name);
+		} else {
+			name[slen-4] = '\0';
+			sprintf(ppmname,"%s.ppm", name);
+			char ts[100];
+			sprintf(ts, "convert %s %s", fname, ppmname);
+			system(ts);
+		}
+		FILE *fpi = fopen(ppmname, "r");
+		if (fpi) {
+			char line[200];
+			fgets(line, 200, fpi);
+			fgets(line, 200, fpi);
+			while (line[0] == '#' || strlen(line) < 2)
+				fgets(line, 200, fpi);
+			sscanf(line, "%i %i", &width, &height);
+			fgets(line, 200, fpi);
+			int n = width * height * 3;
+			data = new unsigned char[n];
+			for (int i=0; i<n; i++)
+				data[i] = fgetc(fpi);
+			fclose(fpi);
+		} else {
+			printf("ERROR opening image: %s\n",ppmname);
+			exit(0);
+		}
+		if (!ppmFlag)
+			return;
+	}
+};
+Image img[1] = {"./ttt.png"};
+
+class GlobalImage {
+public:
+	GLuint tttTexture;
+	int ttt;
+	GlobalImage() {
+		ttt=1;
+	}
+	~GlobalImage() {
+	}
+} nG;
+
+void initTTT()
+{
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glEnable(GL_TEXTURE_2D);
+	//create opengl texture elements
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glGenTextures(1, &nG.tttTexture);
+	glBindTexture(GL_TEXTURE_2D, nG.tttTexture);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3,
+		img[0].width, img[0].height,
+		0, GL_RGB, GL_UNSIGNED_BYTE, img[0].data);
+}
+
+void renderTTT(int x, int y)
+{
+	glColor3f(1.0, 1.0, 1.0);
+	if (nG.ttt) {
+		glBindTexture(GL_TEXTURE_2D, nG.tttTexture);
+		glBegin(GL_QUADS);
+			glTexCoord2f(0.0f, 1.0f); glVertex2i(0, 0);
+			glTexCoord2f(0.0f, 0.0f); glVertex2i(0, y);
+			glTexCoord2f(1.0f, 0.0f); glVertex2i(x, y);
+			glTexCoord2f(1.0f, 1.0f); glVertex2i(x, 0);
+		glEnd();
+	}
+}
 
 void showNagi(int x, int y)
 {
@@ -66,3 +156,4 @@ void checkScores(Rect r, int x, int y)
 	ggprint16(&r, 16, 0xfff0, "HISCORE:%i", hiscore);
 
 }
+
